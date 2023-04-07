6bf7c237cc38064cb6f5edbdb32b0be422e191cf
Chetan Sanjay Shivade <chetanshivade34@gmail.com>
2023-04-07 12:15:18 +0530

    Commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Captcha Form</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href="index.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+   </head>
+   <body>
+      <div class="container">
+         <div class="row">
+            <div class="col-xs-6 col-xs-offset-3">
+               <h1>Contact Form</h1><br/>
+               <form id="frmCaptcha">
+                  <div class="form-group">
+                     <label>Name:</label>
+                     <input style="height:45px;"type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+                  </div>
+                  <div class="form-group">
+                     <label>Mobile:</label>
+                     <input type="text" class="form-control" id="mobile" placeholder="Enter mobile" name="mobile" style="height:45px;">
+                  </div>
+                  <div class="form-group">
+                     <label>Details:</label>
+                     <textarea class="form-control" id="details" placeholder="Enter details" name="details"style="height:85px;"></textarea>
+                  </div>
+				  
+				  <div class="form-group">
+					 <div class="row">
+						<div class="col-lg-10">
+							<label>Captcha:</label>
+							<input type="text" class="form-control" id="captcha" placeholder="Enter captcha" name="captcha">
+						</div>
+						<div class="col-lg-2" style="margin-top:25px;">
+							<img src="captcha.php"/>
+						</div>
+					 </div>	
+                  </div>
+				  
+                  <button type="button" class="btn btn-danger" onclick="submit_data()">Submit</button>
+               </form>
+            </div>
+         </div>
+      </div>
+	  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	  <script>
+	  function submit_data(){
+		jQuery.ajax({
+			url:'insert.php',
+			type:'post',
+			data:jQuery('#frmCaptcha').serialize(),
+			success:function(data){
+				alert(data);
+			}
+		});
+	  }
+	  </script>
+   </body>
+</html>
